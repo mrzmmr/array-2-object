db966a4fefd56c149021ce7364ccb8a496301909
mrzmmr <mrzmmr@gmail.com>
2017-02-08 00:54:33 -0800

    refactor; remove dependencies

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict'
+
+module.exports = function array2Object (array, options, callback) {
+  var defaultSettings
+  var parameters
+  var settings
+  var last
+
+  parameters = [].slice.call(arguments)
+  last = parameters.pop()
+
+  defaultSettings = {
+    value: null,
+    base: {}
+  }
+
+  callback = (
+    last instanceof Function
+    ? last
+    : doop
+  )
+
+  array = (
+    Array.isArray(array)
+    ? array
+    : []
+  )
+
+  options = options || {}
+
+  settings = Object.assign(options, defaultSettings)
+
+  return callback(
+    reduce(array, settings.base, settings.value)
+  )
+
+  function doop (value) {
+    return value
+  }
+
+  function reduce (array, object, value) {
+    return array.reduce(function (accum, curr) {
+      if (!object[curr]) {
+        accum[curr] = value
+      }
+      return accum
+    }, object)
+  }
+}
---
+++ test.js
@@ -0,0 +1,65 @@
+var array2Object = require('./')
+var tape = require('tape')
+
+var array = [ 'one', 'two', 'three' ]
+var object = {three: 3}
+
+var expected1 = {}
+
+tape(
+  'Sync: Should return {}',
+  function (test) {
+    test.deepEqual(expected1, array2Object())
+    test.end()
+  }
+)
+
+tape(
+  'Async: Should return {}',
+  function (test) {
+    array2Object(function (result) {
+      test.deepEqual(expected1, result)
+      test.end()
+    })
+  }
+)
+
+var expected2 = {one: null, two: null, three: null}
+
+tape(
+  'Sync: Should return {one: null, two: null, three: null}',
+  function (test) {
+    test.deepEqual(expected2, array2Object(array))
+    test.end()
+  }
+)
+
+tape(
+  'Async: Should return {one: null, two: null, three: null}',
+  function (test) {
+    array2Object(array, function (result) {
+      test.deepEqual(expected2, result)
+      test.end()
+    })
+  }
+)
+
+var expected3 = {one: null, two: null, three: 3}
+
+tape(
+  'Sync: Should return {one: null, two: null, three: 3}',
+  function (test) {
+    test.deepEqual(expected3, array2Object(array, {base: object}))
+    test.end()
+  }
+)
+
+tape(
+  'Async: Should return {one: null, two: null, three: 3}',
+  function (test) {
+    array2Object(array, {base: object}, function (result) {
+      test.deepEqual(expected3, result)
+      test.end()
+    })
+  }
+)
